e67b2a48a62796820dcf695bcb26ae91d3120a6f
916gaming <153783992+916gaming@users.noreply.github.com>
2025-04-13 17:35:54 -0500

    Update index.js

--- client/index.js
+++ client/index.js
@@ -2,7 +2,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import MonopolyDeal from './MonopolyDeal';
+import MonopolyDeal from './client/MonopolyDeal'; // ❌ WRONG (if not in a subfolder)
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<MonopolyDeal />);
